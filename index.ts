45d239c4f66e1c3d2b0af79e967e16f839066999
Web3-by-Asif <peshawar.asif@gmail.com>
2024-03-23 23:11:39 +0500

    Basic Calc with Switch Statement

---
+++ index.ts
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+do{
+    const answers:{
+    numberOne: number
+    numberTwo: number
+    operator: string
+} = await inquirer.prompt([
+  { type: "number", name: "numberOne", message: "Enter first number: " },
+  { type: "number", name: "numberTwo", message: "Enter second number: " },
+  {
+    type: "list",
+    name: "operator",
+    message: "Select Operator!",
+    choices: ["+", "-", "*", "/"],
+  },
+])
+
+const { numberOne, numberTwo, operator } = answers;
+
+let result
+
+switch (operator) {
+  case "+":
+    result = numberOne + numberTwo;
+    break;
+  case "-":
+    result = numberOne - numberTwo;
+    break;
+  case "*":
+    result = numberOne * numberTwo;
+    break;
+  case "/":
+    result = numberOne / numberTwo;
+    break;
+  default:
+    console.log("Invalid");
+    break;
+}
+
+console.log(chalk.yellow(`${numberOne} ${operator} ${numberTwo} = ${result}`))
+
+var proceed_further = await inquirer.prompt([
+    {
+      input: "input",
+      name: "cont",
+      message: chalk.bgRedBright("\n Do you want to continue?: Yes / No")
+    }
+  ]);
+} while(proceed_further.cont.toLowerCase() === "yes" || proceed_further.cont.toLowerCase() === "y");
+
